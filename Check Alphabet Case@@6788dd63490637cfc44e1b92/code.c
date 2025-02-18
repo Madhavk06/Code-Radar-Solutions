88dae737c8cd846e25e0145eaaa9869fb8e06193
Madhav Kashyap <madhavkashyap05@gmail.com>
2025-02-18 13:27:41 +0530

    Solution to Check Alphabet Case

--- Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
+++ Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
-
 int main() {
     char a;
-    int q;
+    int b=0;
+    int c=0;
     scanf("%c",&a);
-    for(char b='a';b<='z';b++)
+    for(char c='A';c<='Z';c++)
     {
-        if(a==b && a!='a' && a!='e' && a!='i' && a!='o' && a!='u' )
+        if(a==c)
         {
-            printf("Consonant");
-            q=q+1;
+            printf("Uppercase");
+            b=b+1;
+        }
+        
+    }
+    for(char d='a';d<='z';d++)
+    {
+        if(a==d)
+        {
+            printf("Lowercase");
+            c=c+1;
         }
     }
-    if(a=='a'|| a=='e' || a=='i' || a=='o' || a=='u' || a=='A' || a=='E' || a=='I' || a=='O' || a=='U')
-    {
-        printf("Vowel");
-        q=q+1;
-    }
-    else if(a=='0' || a=='1' || a=='2' || a=='3' || a=='4' || a=='5' || a=='6' || a=='7' || a=='8' || a=='9')
-    {
-        printf("Digit");
-        q=q+1;
-    }
-    if(q==0)
-    {
-        printf("Special Character");
+    if(b==0 && c==0){
+        printf("Not an alphabet");
     }
     return 0;
 }
